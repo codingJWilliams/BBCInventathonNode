595b7860173089fdfcc9bde15d7993c32fc6422f
Jay Williams <course@jaywilliams.me>
2017-08-15 14:39:10 +0100

    Better leaderboard

--- index.js
+++ index.js
@@ -1,25 +1,47 @@
 var app = require('express')();
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const fs = require('fs');
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+
+
+function getLeaderboard(callback) {
+  fs.readFile('storage/leaderboard.json', "utf8", function(err, data) {
+    callback(JSON.parse(data))
+  })
+}
+function saveLeaderboard(data){
+  fs.writeFileSync("storage/leaderboard.json", JSON.stringify(data))
+}
+function getTopLeaderboard(callback1){
+  getLeaderboard(function (data) {
+    callback1(data.sort(function (a, b) {
+      if (a.score < b.score) { return -1; };
+      if (a.score > b.score) { return 1; };
+      return 0; }).slice(0, 20)
+    )
+  }
+)
+}
+function saveToLeaderboard(name, score){
+  getLeaderboard(function(data){
+    data.push({"name": name, "score": score})
+    console.log(data)
+    saveLeaderboard(data)
+  })
+}
+
 
 app.get('/', function(req, res){
-  res.sendFile(__dirname + '/html/view.html');
+  res.sendFile(__dirname + '/html/game.html');
 });
-app.get("/master/", function(req, res){
-  res.sendFile(__dirname + "/html/lobby.html")
+app.post("/add-score/", function(req, res){
+  console.log(req.headers.xname, req.headers.xscore)
+  saveToLeaderboard(req.headers.xname, Number(req.headers.xscore));
+  res.end('{"done": true}')
 })
-app.use(require('express').static('assets'))
-io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on("start-connection", function(param){
-    io.emit("thing-started")
-    console.log("START")
-  })
-  socket.on("end-connection", function(param){
-    io.emit("thing-ended")
-    console.log("END")
-  })
-});
+app.use(require('express').static('assets'));
+
 
 http.listen(4000, function(){
   console.log('listening on *:4000');
