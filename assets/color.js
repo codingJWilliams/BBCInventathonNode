4b3b584d275a210b5aa54897019dfb95293f1299
Jay Williams <course@jaywilliams.me>
2017-08-16 11:16:22 +0100

    :+1:

--- assets/color.js
+++ assets/color.js
@@ -5,7 +5,10 @@ function color(imgSrc){                                                         
   img.height = 1000;
   console.log(img)
   var rgb = getAverageRGB(img);
-  return "rgb(" + rgb.r + ", " + rgb.g + ", " + rgb.b + ")"
+  return "rgb(" + Math.floor(convertRange(rgb.r, [ 0, 255 ], [ 0, 200 ] )) + ", " + Math.floor(convertRange(rgb.g, [ 0, 255 ], [ 0, 200 ] )) + ", " + Math.floor(convertRange(rgb.b, [ 0, 255 ], [ 0, 200 ] )) + ")"
+}
+function convertRange( value, r1, r2 ) {
+  return ( value - r1[ 0 ] ) * ( r2[ 1 ] - r2[ 0 ] ) / ( r1[ 1 ] - r1[ 0 ] ) + r2[ 0 ];
 }
 
 function getAverageRGB(imgEl) {
